67e66fc1c9961a83e6d7bd798fae8fa295048816
Ibe Dwi <dwisatriakusuma4@gmail.com>
2022-10-03 02:33:42 +0700

    refactor: extract element editor from page

--- src/pages/basic.tsx
+++ src/pages/basic.tsx
@@ -1,18 +1,17 @@
-import { Container, Heading, Select, Stack, Text } from "@chakra-ui/react";
+import { Container, Heading, Stack } from "@chakra-ui/react";
 import { AddElementButton } from "@components/AddElementButton";
+import { ElementEditor } from "@components/ElementEditor";
 import { ElementRenderer } from "@components/ElementRenderer";
 import { LeafRenderer } from "@components/LeafRenderer";
-import { TextFormattingButton } from "@components/TextFormattingButton";
-import { CustomElement } from "@modules/slate/slateEntity";
+
 import { useCallback, useState } from "react";
-import { createEditor, Descendant, Editor, Node, Transforms } from "slate";
+import { createEditor, Descendant } from "slate";
 import {
   Slate,
   Editable,
   withReact,
   RenderElementProps,
   RenderLeafProps,
-  useSlate,
   useFocused,
   useSlateSelection,
 } from "slate-react";
@@ -88,7 +87,7 @@ function BasicPage() {
           w={600}
           justifyContent="space-between"
         >
-          <ElementEditor editor={editor} />
+          <ElementEditor />
           <Stack borderTopWidth={1} borderBottomWidth={1} py={2} px={3}>
             <Stack direction={"row"}>
               <AddElementButton element="heading" label="Add H1" />
@@ -101,113 +100,6 @@ function BasicPage() {
     </Slate>
   );
 }
-
-const isButton = (editor: Editor, node: Node) => {
-  const [match] = Editor.nodes(editor, {
-    match: (n) => n?.type === "button",
-  });
-
-  return !!match;
-};
-
-const ElementEditor = (props: { editor: Editor }) => {
-  const editor = useSlate();
-  const [match] = Editor.nodes(editor, {
-    match: (n: Node) => {
-      return n?.hasOwnProperty("type");
-    },
-  });
-
-  const getElementType = () => {
-    // Not sure if this the correct way to retrieve Element type
-    const [match] = Editor.nodes(editor, {
-      match: (n: Node) => {
-        return n?.hasOwnProperty("type");
-      },
-    });
-
-    console.log("match", match);
-    const t = match ? (match[0] as CustomElement) : null;
-
-    return t;
-  };
-
-  const changeButtonVariant = (variant: "solid" | "outline" | "ghost") => {
-    Transforms.setNodes(
-      editor,
-      { variant: variant },
-      { match: (n) => Editor.isBlock(editor, n) }
-    );
-  };
-
-  const changeHeadingLevel = (level: 1 | 2) => {
-    Transforms.setNodes(
-      editor,
-      { level: level },
-      { match: (n) => Editor.isBlock(editor, n) }
-    );
-  };
-
-  return (
-    <Stack px={2} py={2}>
-      <Heading fontSize={"2xl"}>Element Editor</Heading>
-      {getElementType()?.type === "heading" && (
-        <>
-          <Stack px={2}>
-            <Select
-              onChange={(e) =>
-                changeHeadingLevel(Number(e.target.value) as 1 | 2)
-              }
-              // can't type narrowing to heading
-              // @ts-ignore
-              value={getElementType()?.level}
-            >
-              <option value={1}>Heading 1</option>
-              <option value={2}>Heading 2</option>
-            </Select>
-          </Stack>
-          <Stack borderTopWidth={1} borderBottomWidth={1} py={3} px={2}>
-            <Text>Text Format</Text>
-            <Stack direction={"row"}>
-              <TextFormattingButton format="bold" />
-              <TextFormattingButton format="underlined" />
-              <TextFormattingButton format="italic" />
-            </Stack>
-          </Stack>
-        </>
-      )}
-      {getElementType()?.type === "paragraph" && (
-        <Stack borderTopWidth={1} borderBottomWidth={1} py={3} px={2}>
-          <Text>Text Format</Text>
-          <Stack direction={"row"}>
-            <TextFormattingButton format="bold" />
-            <TextFormattingButton format="underlined" />
-            <TextFormattingButton format="italic" />
-          </Stack>
-        </Stack>
-      )}
-
-      {getElementType()?.type === "button" && (
-        <Stack px={2}>
-          <Select
-            onChange={(e) =>
-              changeButtonVariant(
-                e.target.value as "solid" | "outline" | "ghost"
-              )
-            }
-            // the Can't type narrowing to ButtonElement
-            // @ts-ignore
-            value={getElementType()?.variant}
-          >
-            <option value={"solid"}>Solid</option>
-            <option value="outline">Outline</option>
-            <option value="ghost">Ghost</option>
-          </Select>
-        </Stack>
-      )}
-    </Stack>
-  );
-};
 
 // Use this to view the editor focused and selected state
 const FocusedBar = () => {
